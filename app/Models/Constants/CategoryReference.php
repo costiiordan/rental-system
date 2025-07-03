571d8b9ecfb548c9cb35e185a2251cd1a28738b6
Constantin Iordan <costi.iordan@gmail.com>
2025-07-03 13:24:53 +0300

    Category management

---
+++ app/Models/Constants/CategoryReference.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models\Constants;
+
+class CategoryReference
+{
+    public const KIDS = 'kids';
+    public const ENDURO_KIDS = 'enduro_kids';
+    public const ELECTRIC_ENDURO = 'electric_enduro';
+    public const SUPER_ENDURO_BIKE_PARK = 'super_enduro_bike_park';
+    public const DOWNHILL = 'downhill';
+    public const ELECTRIC_MOUNTAIN_BIKE = 'electric_mountain_bike';
+}
