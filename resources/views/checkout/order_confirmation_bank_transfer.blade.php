01337f7e04ff99d01e317cf72663d26212ea8af2
Constantin Iordan <costi.iordan@gmail.com>
2026-07-08 22:43:51 +0300

    Update bank transfer details and remove qty discounts.

--- resources/views/checkout/order_confirmation_bank_transfer.blade.php
+++ resources/views/checkout/order_confirmation_bank_transfer.blade.php
@@ -7,8 +7,8 @@
             @lang('Rezervarea a fost salvată cu succes.')<br>
             @lang('Te rugăm să achiți suma de <b>:amount RON</b> în contul nostru bancar:', ['amount' => $order->total])<br>
             <span class="order-confirmation-bank-details">
-                @lang('Iban'): <strong>RO92BACX0000000767636000</strong><br>
-                @lang('Beneficiar'): <strong>Burtan Ciprian Iulius Intreprindere Individuala</strong><br>
+                @lang('Iban'): <strong>RO16 BACX 0000 0024 2358 8000</strong><br>
+                @lang('Beneficiar'): <strong>SC Mediaplay PM SRL</strong><br>
                 @lang('Banca'): <strong>Unicredit Bank S.A.</strong><br>
                 @lang('Detalii plată'): <strong>@lang('Plata rezervare nr :orderId', ['orderId' => $order->id])</strong>
             </span><br><br>
